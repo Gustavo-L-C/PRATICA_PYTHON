54672b546e0d41c9ef3d6d7e7b71967f85ac5f3f
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-05-23 17:56:10 -0300

    Create pratica31.py

---
+++ PRATICA_PYTHON/pratica31.py
@@ -0,0 +1,19 @@
+#23.05.2025
+#Formulário Simples
+
+#Formulário Simples
+
+print('Bem-Vindo\n')
+
+n = str(input('Digite seu nome completo: ')) #entrada de valor (nome)
+e = str(input('Digite seu melhor email: ')) #entrada de valor (email)
+i = int(input('Digite sua idade: ')) #entrada de valor (idade)
+d = int(input('Digite o ano de nascimento: ')) #entrada de valor (data de nascimento)
+c = int(input('Digite seu cpf: ')) #entrada de valor (cpf)
+
+print(f'-----------------------------------\n') #só enfeite
+
+print(f'Seu cadastro\n nome: {n}\n email: {e}\n idade: {i}\n ano: {d}\n cpf: {c}')# imprimir todas as informações coletadas
+print(f'\nSeja Bem-Vindo {n}')
+
+print(f'\n-----------------------------------') #só enfeite
