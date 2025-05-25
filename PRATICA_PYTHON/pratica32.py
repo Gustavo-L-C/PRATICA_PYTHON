e15ca69545e6ae93af9f74e45b9644bf3390b069
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-05-25 16:21:04 -0300

    Create pratica32.py

---
+++ PRATICA_PYTHON/pratica32.py
@@ -0,0 +1,51 @@
+#25.05.2025
+#MÃO LIVRE - 00
+
+#leia dois valoras entre horas, minutos e segundos e transforme
+# 1- horas em minutos
+# 2- minutos em segundos
+# 3- segundos em minutos
+# 4- minutos em horas:
+# depois imprima o resultado
+
+# 01 horas = 60 minutos = 3600 segundos
+
+t = 60
+s = 3600
+
+oqt1 = str(input('o que vamos transformar (horas, minutos, segundos)? '))
+oqt2 = str(input('Em que iremos transformar (horas, minutos, segundos)? '))
+
+
+
+if oqt1 == 'horas' and oqt2 == 'minutos': #1-horas em minutos
+    qh1= int(input('Digite a quantidade de horas: '))
+
+    hm = qh1 * t
+
+    print(f'{qh1} horas são {hm} minutos')
+
+elif oqt1 == 'minutos' and oqt2 == 'segundos': #2-minutos em segundos
+    qh2= int(input('Digite a quantidade de minutos: '))
+
+    hs = qh2 * s
+
+    print(f'{qh2} minutos são {hs} segundos')
+
+if oqt1 == 'segundos' and oqt2 == 'minutos': #3-segundos em minutos
+    qh3= int(input('Digite a quantidade de segundos: '))
+
+    sm= qh3 / t
+
+    print(f'{qh3} segundos são {sm} minutos')
+
+elif oqt1 == 'minutos' and oqt2 == 'horas': #4-minutos em horas
+    qh4= int(input('Digite a quantidade de minutos: '))
+
+    mh = qh4 / t
+
+    print(f'{qh4} minutos são {mh} horas')
+
+
+
+
