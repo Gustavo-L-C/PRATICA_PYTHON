7bed0769e379cf426281cccac7626acce3e12d58
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-05-27 18:41:47 -0300

    Update pratica31.py

--- PRATICA_PYTHON/pratica31.py
+++ PRATICA_PYTHON/pratica31.py
@@ -1,7 +1,8 @@
 #23.05.25
 #Formulário Simples
 
-#Formulário Simples
+
+#Faça um formulário Simples que leia o nome, email, idade, ano de nascimento e cpf de uma pessoa e depois a imprima essas inforamções:
 
 print('Bem-Vindo\n')
 
@@ -11,9 +12,12 @@ i = int(input('Digite sua idade: ')) #entrada de valor (idade)
 d = int(input('Digite o ano de nascimento: ')) #entrada de valor (data de nascimento)
 c = int(input('Digite seu cpf: ')) #entrada de valor (cpf)
 
+
 print(f'-----------------------------------\n') #só enfeite
 
 print(f'Seu cadastro\n nome: {n}\n email: {e}\n idade: {i}\n ano: {d}\n cpf: {c}')# imprimir todas as informações coletadas
-print(f'\nSeja Bem-Vindo {n}')
 
 print(f'\n-----------------------------------') #só enfeite
+
+print('Cadastro finalizado com sucesso!')
+print(f'\nSeja Bem-Vindo {n}')
