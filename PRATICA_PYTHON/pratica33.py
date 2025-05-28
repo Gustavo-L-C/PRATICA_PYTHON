542db3ec96e8674436800d5b1794e7662fcec490
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-05-28 17:32:21 -0300

    Update pratica33.py

--- PRATICA_PYTHON/pratica33.py
+++ PRATICA_PYTHON/pratica33.py
@@ -1,6 +1,6 @@
 #26.05.25
 #jogo da Adivinhação com random.randint.
-
+import random
 from random import randint
 
 def JA():
@@ -14,9 +14,11 @@ def JA():
     premio = '🎁'
     t = 0
     p = 0
+    print(f'\n Vamos começar {nome}!!')
+
 
     while p != n:
-        print(f'\n Vamos começar {nome}!!')
+
         try:
 
             p = int(input(f'Qual seu palpite {nome}(entre 1 e 20)? '))
@@ -24,24 +26,24 @@ def JA():
 
 
             if p < n:
-                print(f'Tente um número maior {nome}.')
+                print(f'\033[34mTente um número maior {nome}.\033[m')
 
             elif p > n:
-                print(f'Tente um número menor {nome}.')
+                print(f'\033[35mTente um número menor {nome}.\033[m')
 
             else:
-                print(f'\nBoa {nome} você acertou o número {n} em {t} tentativas.\n')
+                print(f'\n\033[33mBoa {nome} você acertou o número \033[32m{n}\033[33m em \033[32m{t}\033[33m tentativas.\n\033[m')
                 print(f'Tome seu prêmio {nome}.{premio}\n')
 
                 ap = str(input('Quer abrir seu prêmio(S/N)? '))
                 if ap == 'S' or 's':
-                    print('\n⚔️⚔️Toma Elden Ring Night Rein⚔️⚔️')
+                    print('\n⚔️⚔️Toma \033[34mElden Ring Night Rein\033[m⚔️⚔️')
                 else:
                     print('\nOK.')
 
 
         except ValueError:
-            print('Número Inválido...')
+            print('\033[31mNúmero Inválido..\033[m')
 
     print(f'Obrigado por jogar {nome}!')
 JA()
