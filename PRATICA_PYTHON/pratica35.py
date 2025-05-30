1b67c2809824303bc44a9061f87570aa003259f6
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-05-30 18:28:10 -0300

    Create pratica35.py

---
+++ PRATICA_PYTHON/pratica35.py
@@ -0,0 +1,19 @@
+#30.05.25
+#ESTRUTURAS DE REPETIÇÂO - 01
+from random import randint
+
+#FOR - 01
+l1 = 1
+l2 = 100
+
+for ran in range(10):
+
+    ran = randint(l1, l2)
+
+    if ran >= 90:
+        print(f'sorteado {ran}')
+
+    else:
+        print(f'numero {ran}')
+
+print('Acabou')
