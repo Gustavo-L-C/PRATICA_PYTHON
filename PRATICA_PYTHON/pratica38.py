b2d06bd8f24e27c06a9a32abe43b3108af87a10f
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-01 19:34:32 -0300

    Update pratica38.py

--- PRATICA_PYTHON/pratica38.py
+++ PRATICA_PYTHON/pratica38.py
@@ -3,15 +3,19 @@
 
 
 #FOR - 03
+print('_________________________________________________________________________________________________________________')
 
-v1 = float(input("Insira o valor: R$ "))
-v2 = float(input("Insira o valor: R$ "))
-v3 = float(input("Insira o valor: R$ "))
+v1 = float(input("1- Insira o valor: R$ "))
+v2 = float(input("2- Insira o valor: R$ "))
+v3 = float(input("3- Insira o valor: R$ "))
 # Lê três valores
 
+print('_________________________________________________________________________________________________________________')
+print('\033[32mValor do Item\033[m / \033[33mTaxa\033[m / \033[34mValor Total\033[m / \033[31mImposto\033[m') # só para organizar os valores
 valores = [v1, v2, v3] # Os valores foram agrupados em uma lsita
 
 total_de_impostos = 0
+n = 0
 
 for item in valores: # Para cada item dentro da lista de valores
 
@@ -30,11 +34,14 @@ for item in valores: # Para cada item dentro da lista de valores
     valor_completo = imposto + item
     # calculo total do item + imposto
 
-    print(f'\nO valor R${item:.2f}, recebeu um imposto de {taxa * 100:.0f}%, ficando R${valor_completo:.2f}')
-    print(f'Sendo o valor do imposto R${imposto:.2f} + o valor do item R${item:.2f} = R${valor_completo:.2f}\n')
+    n += 1 # adicionado uma numeração para cada repetição
+
+    print(f'\n{n}- O item no valor de \033[32mR${item:.2f}\033[m, recebeu uma taxa de \033[33m{taxa * 100:.0f}%\033[m, ficando \033[34mR${valor_completo:.2f}\033[m')
+    print(f'Sendo o valor do item \033[32mR${item:.2f}\033[m + o valor do imposto \033[31mR${imposto:.2f}\033[m = \033[34mR${valor_completo:.2f}\033[m')
 
     total_de_impostos += imposto
     # calcular o total de imposto dos três itens isolados e somados
 
 
-print(f'O total de impostos de todos os itens é de \033[31mR${total_de_impostos:.2f}\033[m')
+print(f'\nO total de impostos de todos os itens é de \033[31mR${total_de_impostos:.2f}\033[m')
+print('_________________________________________________________________________________________________________________')
