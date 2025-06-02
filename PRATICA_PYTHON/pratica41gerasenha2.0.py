ec6fab08a6e3c258ee48b3c871ef2eaeac63749c
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-02 19:26:49 -0300

    Update pratica41gerasenha2.0.py

--- PRATICA_PYTHON/pratica41gerasenha2.0.py
+++ PRATICA_PYTHON/pratica41gerasenha2.0.py
@@ -5,39 +5,41 @@
 
 from random import randint
 
+repetir = 0
 s1 = 1
 s2 = 60
-
-se1 = randint(s1, s2)
-se2 = randint(s1, s2)
-se3 = randint(s1, s2)
-se4 = randint(s1, s2)
-se5 = randint(s1, s2)
-se6 = randint(s1, s2)
-
-senha = [se1, se2, se3, se4, se5, se6]
 
 print('_________________________________________________________________________________________________________________')
 
 print('\n\033[34m--Gerador de senhas--\033[m\n')
+
 nome = str(input('Qual seu nome? '))
-quantidade = int(input('De quantos senhas precisará? '))
-
-print('_________________________________________________________________________________________________________________')
-
-repetir = 0
 
 nomes = ['Gustavo', 'GUSTAVO', 'gustavo']
 
-for numero in range(quantidade):
+print('_________________________________________________________________________________________________________________')
 
-    repetir += 1
+if nome in nomes:
     print(f'\nSeja bem vindo {nome}!')
 
-    if nome in nomes:
+    quantidade = int(input('De quantos senhas precisará? '))
+
+    for numero in range(quantidade):
+
+        repetir += 1
+
+        se1 = randint(s1, s2)
+        se2 = randint(s1, s2)
+        se3 = randint(s1, s2)
+        se4 = randint(s1, s2)
+        se5 = randint(s1, s2)
+        se6 = randint(s1, s2)
+
+        senha = [se1, se2, se3, se4, se5, se6]
+
         print(f'{repetir}- A senha do dia é: {senha}\n')
 
-    else:
-        print('Tchau!')
+else:
+    print('Tchau!')
 
 print('_________________________________________________________________________________________________________________')
