b423eed640edc82486dbb9fc0eab095e8c0e629b
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-03 16:59:47 -0300

    Create pratica42.py

---
+++ PRATICA_PYTHON/pratica42.py
@@ -0,0 +1,15 @@
+#03.06.25
+#ESTRUTURAS DE REPETIÇÃO - 05
+
+#WHILE - 02
+
+n1 = int(input('digite um numero: ')) # escolhe a quantidade de repetições
+
+n = 0
+
+while n < n1: # o while só parará quando igual a n1
+
+    n += 1
+
+    print(f'{n}- hello world')
+
