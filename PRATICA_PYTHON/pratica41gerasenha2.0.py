23950af76e52068eb36927acf5c5d539542f4522
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-03 12:21:19 -0300

    Update pratica41gerasenha2.0.py

--- PRATICA_PYTHON/pratica41gerasenha2.0.py
+++ PRATICA_PYTHON/pratica41gerasenha2.0.py
@@ -7,16 +7,22 @@ from random import randint # importando o radint
 
 repetir = 0 # numerção na repetição
 
-s1 = 1 # numero minimo
-s2 = 60 # numero maximo
-
 print('_________________________________________________________________________________________________________________') # só decoração
 
 print('\n\033[34m--Gerador de senhas--\033[m\n') # só decoração
 
 nome = str(input('Qual seu nome? ')) # Recebe um nome
 
+se2 = int(input(f'{nome} escolha até que número sera usado para a senha (sempre será partindo do 1, ex: 1 a \033[34m10\033[m): ')) # escolher o "range" numerico que será utilizado para a senha
+
 nomes = ['Gustavo', 'GUSTAVO', 'gustavo'] # lista de nomes que possuem acesso
+
+print('_________________________________________________________________________________________________________________') # só decoração
+
+s1 = 1 # numero minimo
+s2 = se2 # numero maximo
+
+print(f'A senha será de {s1} a {s2}.') # só decoraçâo
 
 print('_________________________________________________________________________________________________________________') # só decoração
 
@@ -29,11 +35,11 @@ if nome in nomes: # se o nome recebido está dentro da lista de nomes
 
         repetir += 1 # numerção na repetição
 
-        se1 = randint(s1, s2)
+        se1 = randint(s1, se2)
         se2 = randint(s1, s2)
-        se3 = randint(s1, s2)
+        se3 = randint(s1, se2)
         se4 = randint(s1, s2)
-        se5 = randint(s1, s2)
+        se5 = randint(s1, se2)
         se6 = randint(s1, s2)
         # numero aleatorio dentro do "range" pre estabelecido
 
