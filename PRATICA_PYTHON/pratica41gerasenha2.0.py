776ce693b1e25559308012b25c3e1ef9ecf202d4
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-03 16:18:29 -0300

    Update pratica41gerasenha2.0.py

--- PRATICA_PYTHON/pratica41gerasenha2.0.py
+++ PRATICA_PYTHON/pratica41gerasenha2.0.py
@@ -13,7 +13,7 @@ print('\n\033[34m--Gerador de senhas--\033[m\n') # só decoração
 
 nome = str(input('Qual seu nome? ')) # Recebe um nome
 
-se2 = int(input(f'{nome} escolha até que número sera usado para a senha (sempre será partindo do 1, ex: 1 a \033[34m10\033[m): ')) # escolher o "range" numerico que será utilizado para a senha
+se2 = int(input(f'{nome} escolha até que número sera usado para a senha (de 1 a \033[34msua escolha\033[m, sempre será partindo de 1): ')) # escolher o "range" numerico que será utilizado para a senha
 
 nomes = ['Gustavo', 'GUSTAVO', 'gustavo'] # lista de nomes que possuem acesso
 
@@ -22,7 +22,7 @@ print('_________________________________________________________________________
 s1 = 1 # numero minimo
 s2 = se2 # numero maximo
 
-print(f'A senha será de {s1} a {s2}.') # só decoraçâo
+print(f'A senha será de {s1} a {s2}.') # mostra o "range" numerico da senha
 
 print('_________________________________________________________________________________________________________________') # só decoração
 
@@ -45,7 +45,8 @@ if nome in nomes: # se o nome recebido está dentro da lista de nomes
 
         senha = [se1, se2, se3, se4, se5, se6] # senha gerada de 6 numeros aleatorios para cada repetição
 
-        print(f'{repetir}- A senha do dia é: {senha}\n') # imprimi a/as senhas geradas
+        print(f'{repetir}- A senha do dia é: {senha}\n') # mostra a/as senhas geradas
+        # print(f'{repetir}- A senha do dia é: {se1, se2, se3, se4, se5, se6}\n') # outro jeito de mostrar
 
 else: # caso nome não esteja na lista
     print(f'\nTchau {nome}!') # tchau
