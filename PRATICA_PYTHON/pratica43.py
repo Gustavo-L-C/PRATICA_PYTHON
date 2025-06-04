da330729ae4bbd039586aa9e27a268081e8b1ea8
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-04 17:27:56 -0300

    Update pratica43.py

--- PRATICA_PYTHON/pratica43.py
+++ PRATICA_PYTHON/pratica43.py
@@ -4,17 +4,17 @@
 #FOR - 04
 #PRATICA
 
-matriz = [[1,2,3],[4,5,6],[7,8,9],[0]]
+matriz = [[1,2,3],[4,5,6],[7,8,9],[0]] # matriz
 
 print('_________________________________________________________________________________________________________________')
 
-for item in matriz[0]:
-    print(item)
-
+for item in matriz[0]: # para cada item dentro da (item na posição 0)
+    print(item) # imprimi item
 
 print('_________________________________________________________________________________________________________________')
 
-for linha in matriz:
-    print('---')
-    for coluna in linha:
-        print(coluna)
+for linha in matriz: # para cada linha dentro de matriz
+    print('---') # imprimi linha
+    for coluna in linha: # para cada coluna dentro de linha
+        print(coluna) # imprimi coluna
+# utilizando um for dentro de outro
