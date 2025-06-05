10e18e7f60b48b521c46b445e673c6a04abc6fe5
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-05 18:27:32 -0300

    Create bancodenomes.py

---
+++ PRATICA_PYTHON/bancodenomes.py
@@ -0,0 +1,4 @@
+#05.06.25
+#banco de nomes para facilitar
+
+bancos_nomes = ['Gustavo', 'Gus', 'gustavo', 'gus', 'GUSTAVO', 'GUS', 'G', 'g'] # adicione os nomes a lista para utilizar como um "banco de cadastros"
