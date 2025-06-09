ffa545534256f5d1d84e060d52c4529742aa075b
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-09 18:23:51 -0300

    Update pratica47.py

--- PRATICA_PYTHON/pratica47.py
+++ PRATICA_PYTHON/pratica47.py
@@ -15,9 +15,17 @@ nome()
 n1 = int(input('Digite um numero: '))
 n2 = int(input('Digite outro numero: '))
 
-def soma(n1, n2):
+def soma1(n1, n2):
+    soma1 = n1 + n2
+    print(soma1)
+soma1(n1, n2)
 
-    soma = n1 + n2
-    print(soma)
+#---------------------------------------------------------------------------
 
-soma(n1, n2)
+def soma2(n1, n2):
+    soma2 = n1 + n2
+    return soma2
+resultado = soma2(n1, n2)
+print(resultado)
+
+#---------------------------------------------------------------------------
