f15c49d5742a007252fa519ea0550c46c3ea75e3
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-09 18:36:19 -0300

    Update pratica48.py

--- PRATICA_PYTHON/pratica48.py
+++ PRATICA_PYTHON/pratica48.py
@@ -7,7 +7,6 @@
 # coletar a nota de uma turma de 10 alunos
 # e dar a média de nota da turma
 
-
 n1 = float(input('Digite a nota 1: '))
 n2 = float(input('Digite a nota 2: '))
 n3 = float(input('Digite a nota 3: '))
@@ -18,18 +17,25 @@ n7 = float(input('Digite a nota 7: '))
 n8 = float(input('Digite a nota 8: '))
 n9 = float(input('Digite a nota 9: '))
 n10 = float(input('Digite a nota 10: '))
+# coletamos as notas
 
 turma = [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10]
+# agrupamos as notas
 
-def media_turma(turma):
+def media_turma(turma): # definimos a media da turma
 
-    qtd = len(turma)
-    soma_notas = 0
+    qtd = len(turma) # pegamos a quantidade de alunos
 
-    for notas in turma:
-        soma_notas += notas
-    media_notas = soma_notas/qtd
+    soma_notas = 0 # para a soma
+
+    for notas in turma: # para cada nota na turma
+
+        soma_notas += notas # somando as notas
+
+    media_notas = soma_notas/qtd # calculando a média
+
     return media_notas
 
 media_da_turma = media_turma(turma)
-print(f'A média de nota da turma é {media_da_turma}')
+
+print(f'A média de nota da turma é {media_da_turma}') # mostrando a média 
