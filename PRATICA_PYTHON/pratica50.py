e7b2eed52407d090bba40e8c5a90118efeba116c
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-12 19:18:24 -0300

    Update pratica50.py

--- PRATICA_PYTHON/pratica50.py
+++ PRATICA_PYTHON/pratica50.py
@@ -3,11 +3,11 @@
 
 # DEF - 03
 
-quantos = int(input('Digite um número para a contagem regressiva: '))
+quantos = int(input('Digite um número para a contagem regressiva: ')) # coleta o numero para a contagem "regressiva"
 
-def contagem_regressiva(numero):
+def contagem_regressiva(numero): # função contagem regressiva recebe um numero
 
-    for i in range(numero, -1, -1):
-        print(i)
+    for i in range(numero, -1, -1): # para cada item no "range" de numero, -1 numero, -1 numero
+        print(i) # imprimi item
 
-contagem_regressiva(quantos)
+contagem_regressiva(quantos) # recebe o numero da contagem "regressiva"
