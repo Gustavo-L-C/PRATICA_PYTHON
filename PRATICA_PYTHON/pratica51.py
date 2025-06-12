b9490e0f988e7dbdc75e406cf6907d9260614565
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-12 19:15:42 -0300

    Update pratica51.py

--- PRATICA_PYTHON/pratica51.py
+++ PRATICA_PYTHON/pratica51.py
@@ -17,7 +17,7 @@ def subtracao(): # SUBTRAÇÃO
 
 def multiplicacao(): # MULTIPLICAÇÃO
     multiplicar = s1 * s2
-    print(f'A {esc} entre {s1:.2f} e {s2:.2f} é igual a {multiplicar:.2f}')
+    print(f'A {esc} entre {s1:.2f} e {s2:.2f} é igual a {multiplicar:.2f}') # imprimi o resultado
 
 def divisao(): # DIVISÃO
     dividir = s1 / s2
