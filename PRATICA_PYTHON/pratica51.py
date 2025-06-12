fba4bc64498179b7378ba510d8bc64d5917f54a1
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-12 19:09:48 -0300

    Create pratica51.py

---
+++ PRATICA_PYTHON/pratica51.py
@@ -0,0 +1,48 @@
+#12.06.25
+#FUNÇÕES - 04
+
+# DEF - 04
+
+def soma():
+    somar = s1 + s2
+    print(f'A {esc} entre {s1:.2f} e {s2:.2f} é igual a {somar:.2f}')
+
+def subtracao():
+    subtrair = s1 - s2
+    print(f'A {esc} entre {s1:.2f} e {s2:.2f} é igual a {subtrair:.2f}')
+
+def multiplicacao():
+    multiplicar = s1 * s2
+    print(f'A {esc} entre {s1:.2f} e {s2:.2f} é igual a {multiplicar:.2f}')
+
+def divisao():
+    dividir = s1 / s2
+    print(f'A {esc} entre {s1:.2f} e {s2:.2f} é igual a {dividir:.2f}')
+
+s1 = float(input('Digite um numero: '))
+s2 = float(input('Digite outro numero: '))
+
+esc = str(input('Qual tipo de calculo deseja executar? '))
+
+som = ['soma','Soma','SOMA']
+sub = ['subtração','Subtração','SUBTRAÇÃO']
+mul = ['multiplicação','Multiplicação','MULTIPLICAÇÃO']
+div = ['divisão','Divisão','DIVISÃO']
+
+if esc in som:
+    soma()
+
+if esc in sub:
+    subtracao()
+
+if esc in mul:
+    multiplicacao()
+
+if esc in div:
+    divisao()
+
+else:
+    print('Invalido')
+
+
+
