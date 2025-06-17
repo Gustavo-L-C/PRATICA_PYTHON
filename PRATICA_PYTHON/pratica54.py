2b5ff2e77795fa00d600f9e9a7f20e5222abff1d
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-17 20:05:32 -0300

    Create pratica54.py

---
+++ PRATICA_PYTHON/pratica54.py
@@ -0,0 +1,23 @@
+#17.06.25
+
+#PRATICA
+
+from bancodenomes import bancos_nomes
+
+nome1 = str(input('\nQual seu nome? '))
+
+
+class OLA:
+
+    def ola(self, nome):
+        self.nome = nome
+
+    def imprimir(self):
+
+        if nome1 in bancos_nomes:
+            print('\033[33mOlá seja bem-vindo,', nome1, '\033[m')
+        else:
+            print('Seja bem-vindo,', nome1)
+
+ola = OLA()
+ola.imprimir()
