c4e4ad6d6180be16f9d319d547046fe60b57a350
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-17 16:45:09 -0300

    Update pratica53.py

--- PRATICA_PYTHON/pratica53.py
+++ PRATICA_PYTHON/pratica53.py
@@ -7,25 +7,19 @@ from bancodenomes import bancos_nomes # banco de nomes
 nome1 = str(input('Digite seu nome: ')) # coletar o nome
 
 if nome1 in bancos_nomes: # se o nome coletado estiver dentro do banco de nomes
-
     salario1 = 2000 # salario igual a 2000
 
 else: # se não
-
     salario1 = 1500 # salario igual a 1500
 
 class Emp: # classe Emp (empregado)
 
     def __init__(self, nome, salario): # função inicial nome e salario
-
         self.nome = nome
-
         self.salario = salario
 
     def imprime(self): # função para imprimir na tela
-
         print(f'{self.nome} tem um salário de R$ {self.salario:.2f}')
 
 emp = Emp(nome1, salario1)
-
 emp.imprime()
