b1e3ff9abfd8048a3b68b3565ab0b81f266674a0
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-17 16:28:02 -0300

    Create pratica53.py

---
+++ PRATICA_PYTHON/pratica53.py
@@ -0,0 +1,23 @@
+#17.06.25
+
+#PRATICA
+
+from bancodenomes import bancos_nomes
+
+nome1 = str(input('Digite seu nome: '))
+
+if nome1 in bancos_nomes:
+    salario1 = 2000
+
+else:
+    salario1 = 1500
+
+class Emp:
+    def __init__(self, nome, salario):
+        self.nome = nome
+        self.salario = salario
+    def imprime(self):
+        print(f'{self.nome} tem um salário de R$ {self.salario:.2f}')
+
+emp = Emp(nome1, salario1)
+emp.imprime()
