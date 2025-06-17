683d01b127dbcb4db7877ece8b3c066a8be9f310
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-06-17 20:06:18 -0300

    Update pratica54.py

--- PRATICA_PYTHON/pratica54.py
+++ PRATICA_PYTHON/pratica54.py
@@ -2,22 +2,32 @@
 
 #PRATICA
 
-from bancodenomes import bancos_nomes
+from bancodenomes import bancos_nomes # banco de nomes
 
-nome1 = str(input('\nQual seu nome? '))
+# _________________________________________________________________________________________________________________
 
+nome1 = str(input('Digite seu nome: ')) # coletar o nome
 
-class OLA:
+# _________________________________________________________________________________________________________________
 
-    def ola(self, nome):
+if nome1 in bancos_nomes: # se o nome coletado estiver dentro do banco de nomes
+    salario1 = 2000 # salario igual a 2000
+
+else: # se não
+    salario1 = 1500 # salario igual a 1500
+
+# _________________________________________________________________________________________________________________
+
+class Emp: # classe Emp (empregado)
+
+    def __init__(self, nome, salario): # função inicial nome e salario
         self.nome = nome
+        self.salario = salario
 
-    def imprimir(self):
+    def imprime(self): # função para imprimir na tela
+        print(f'{self.nome} tem um salário de R$ {self.salario:.2f}')
 
-        if nome1 in bancos_nomes:
-            print('\033[33mOlá seja bem-vindo,', nome1, '\033[m')
-        else:
-            print('Seja bem-vindo,', nome1)
+emp = Emp(nome1, salario1)
+emp.imprime()
 
-ola = OLA()
-ola.imprimir()
+# _________________________________________________________________________________________________________________
