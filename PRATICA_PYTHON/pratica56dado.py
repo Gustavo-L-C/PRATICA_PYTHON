d46351f15dd93cfa62a75154c79df012abfae50d
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-07-31 17:35:14 -0300

    Update pratica56dado.py

--- PRATICA_PYTHON/pratica56dado.py
+++ PRATICA_PYTHON/pratica56dado.py
@@ -14,9 +14,10 @@ ng = 0
 print(f'{dd} Jogo de Dados {dd}\n')
 
 tipo = int(input('Digite o tipo de dado (ex: digite 20 para dado D20): '))
-print(f'Tipo de Dado escolhido: D{tipo}')
+print(f'Tipo de Dado escolhido: D{tipo}\n')
 
 giros = int(input('Digite a quantidade de giros: '))
+print(' ')
 
 for giro in range(giros):
 
