3d31a53f5bb254a8b03665fc5cdeaa786d3eb59e
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-07-31 17:50:19 -0300

    Update pratica55caracoroa.py

--- PRATICA_PYTHON/pratica55caracoroa.py
+++ PRATICA_PYTHON/pratica55caracoroa.py
@@ -2,23 +2,27 @@
 
 from random import randint
 
-print('😐 Cara ou Coroa 👑\n')
+co = '👑'
+ca = '😐'
+mo = '🪙'
+
+print(f'{ca} Cara ou Coroa {co}\n')
 
 melhor = int(input('Quantas Vezes quer jogar: '))
 
 print(' ')
 
-op1 = input('😐- O que será Cara: ')
-op2 = input('👑- O que será Coroa: ')
+op1 = input(f'{ca}- O que será Cara: ')
+op2 = input(f'{co}- O que será Coroa: ')
 
 for quantidade in range(melhor):
 
     cc = randint(1,2)
 
     if cc == 1:
-        print('\nGirando 🪙🪙🪙')
-        print('😐 Cara', op1)
+        print(f'\nGirando {mo * 3}')
+        print(f'{ca} Cara', op1)
 
     elif cc == 2:
-        print('\nGirando 🪙🪙🪙')
-        print('👑 Coroa', op2)
+        print(f'\nGirando {mo * 3}')
+        print(f'{co} Coroa', op2)
