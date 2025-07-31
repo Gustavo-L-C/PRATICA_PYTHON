19ce91fc2bb6421b0a97a68bdd0bc8dc47de82d6
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-07-31 13:11:08 -0300

    Update pratica56dado.py

--- PRATICA_PYTHON/pratica56dado.py
+++ PRATICA_PYTHON/pratica56dado.py
@@ -1,1 +1,13 @@
-# DADO 
+# DADO
+
+from random import randint
+
+# receba o tipo de dado d6, d20, d100, ...
+# gire o dado escolhido quantas vezes for decidido
+# receber quantas vezes o dado será lançado
+# por na tela os resultados do giro ou de cada giro do dado
+
+
+tipo = int(input("Digite o tipo de dado (ex: digite 20 para dado D20): "))
+
+print(f"Tipo escolhido: D{tipo}")
