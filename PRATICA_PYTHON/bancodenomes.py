70cc1bea2394bc7e3812fb6df15e06dfb11b9a28
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-08-01 15:26:14 -0300

    Update bancodenomes.py

--- PRATICA_PYTHON/bancodenomes.py
+++ PRATICA_PYTHON/bancodenomes.py
@@ -2,3 +2,4 @@
 #banco de nomes para facilitar algumas coisas
 
 bancos_nomes = ['Gustavo', 'Gus', 'gustavo', 'gus', 'GUSTAVO', 'GUS', 'G', 'g'] # adicione os nomes a lista para utilizar como um "banco de cadastros"
+sim_nao = ['s', 'S', 'ss', 'SS', 'Ss', 'sS', 'sim', 'SIM', 'Sim', 'n', 'N', 'nao', 'NAO', 'Nao', 'não', 'NÃO', 'Não']
