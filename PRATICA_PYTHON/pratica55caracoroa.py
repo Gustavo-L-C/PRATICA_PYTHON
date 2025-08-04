e4618264f8885a159e5b9484bd7db50cf2d227f2
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-08-04 18:15:24 -0300

    Update pratica55caracoroa.py

--- PRATICA_PYTHON/pratica55caracoroa.py
+++ PRATICA_PYTHON/pratica55caracoroa.py
@@ -7,7 +7,7 @@ from time import sleep
 LE = ['👑', '😐', '🪙']
 
 
-print(f'{LE[1]} Cara ou Coroa {co}\n')
+print(f'{LE[1]} Cara ou Coroa {LE[0]}\n')
 
 melhor = int(input('Quantas Vezes quer jogar: '))
 
