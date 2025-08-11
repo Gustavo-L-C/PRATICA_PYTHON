ca7fff61f21d61f78d0c8367d28d839d699f1e3b
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-08-11 11:51:34 -0300

    Update pratica57.py

--- PRATICA_PYTHON/pratica57.py
+++ PRATICA_PYTHON/pratica57.py
@@ -4,52 +4,52 @@
 from random import randint
 from time import sleep
 
-LE = ['🐲', '💀', '🍀', '☠️', '🎲']
+Lista_Emoji = ['🐲', '💀', '🍀', '☠️', '🎲']
 
-N = 0
+Num_Tentativas = 0
 
-print(f'{LE[0]} Rolamento D20 D&D {LE[0]}\n')
+print(f'{Lista_Emoji[0]} Rolamento D20 D&D {Lista_Emoji[0]}\n')
 
-CD = int(input('Qual é Classe de Dificuldade do momento: '))
+Classe_Dificuldade = int(input('Qual é Classe de Dificuldade do momento: '))
 
-NR = int(input('Digite o Número de Rolamentos: '))
+Num_Rolamentos = int(input('Digite o Número de Rolamentos: '))
 
 print(' ')
 
-for pontos in range(NR):
+for pontos in range(Num_Rolamentos):
 
-    if NR <= 10:
+    if Num_Rolamentos <= 10:
         sleep(0.5)
-        print(f'{LE[4]}', end='')
+        print(f'{Lista_Emoji[4]}', end='')
 
-    elif NR >= 50:
+    elif Num_Rolamentos >= 50:
         sleep(0.12)
-        print(f'{LE[4]}', end='')
+        print(f'{Lista_Emoji[4]}', end='')
 
     else:
         sleep(0.25)
-        print(f'{LE[4]}', end='')
+        print(f'{Lista_Emoji[4]}', end='')
 
 print('\n')
 
-for rolamentos in range(NR):
+for rolamentos in range(Num_Rolamentos):
 
-    RDD = randint(1, 20)
-    N += 1
+    Rolamento_Dados = randint(1, 20)
+    Num_Tentativas += 1
 
-    if RDD >= CD:
-        if RDD == 20:
-            print(f'{N}- {RDD} SUCESSO CRÍTICO {LE[2]*3}')
+    if Rolamento_Dados >= Classe_Dificuldade:
+        if Rolamento_Dados == 20:
+            print(f'{Num_Tentativas}- {Rolamento_Dados} SUCESSO CRÍTICO {Lista_Emoji[2]*3}')
             sleep(0.4)
 
         else:
-            print(f'{N}- {RDD} SUCESSO {LE[0]*3}')
+            print(f'{Num_Tentativas}- {Rolamento_Dados} SUCESSO {Lista_Emoji[0]*3}')
             sleep(0.4)
 
-    elif RDD == 1:
-        print(f'{N}- {RDD} FALHA CRITICA {LE[3]*3}')
+    elif Rolamento_Dados == 1:
+        print(f'{Num_Tentativas}- {Rolamento_Dados} FALHA CRITICA {Lista_Emoji[3]*3}')
         sleep(0.4)
 
     else:
-        print(f'{N}- {RDD} FALHO {LE[1]*3}')
+        print(f'{Num_Tentativas}- {Rolamento_Dados} FALHO {Lista_Emoji[1]*3}')
         sleep(0.4)
