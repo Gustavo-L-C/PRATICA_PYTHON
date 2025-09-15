c7f52ca8a5ac6943cdfac3c95dcb451f48837f97
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-09-15 12:09:37 -0300

    Update pratica58.py

--- PRATICA_PYTHON/pratica58.py
+++ PRATICA_PYTHON/pratica58.py
@@ -11,16 +11,23 @@ print(f'{eR} Conversor horas em anos e anos em horas {eC}\n')
 print('O que você quer converter? ')
 qual = int(input('Digite (1) para anos em horas e (2) para horas em anos: '))
 
-anos = int(input('Digite quantos anos: '))
-
 um_ano = 365
 ano_hora = 8766
 
 if qual == 1:
+    anos = int(input('Digite quantos anos: '))
 
     ano_p_hora = anos * ano_hora
 
     print(f'{anos} anos são {ano_p_hora} horas.')
+
+else:
+    if qual == 2:
+        horas = float(input('Digite quantos horas: '))
+
+        hora_p_ano = horas * ano_hora
+
+        
 
 #hora_p_ano = hora / ano_hora
 #ano_p_hora = anos * ano_hora
