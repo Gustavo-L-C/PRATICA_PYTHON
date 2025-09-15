534aafa05b85477769a2070bfd596cbcb3dc3fc9
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2025-09-15 11:52:38 -0300

    Update pratica58.py

--- PRATICA_PYTHON/pratica58.py
+++ PRATICA_PYTHON/pratica58.py
@@ -1,9 +1,14 @@
 #08.08.25
 
 hora = float(input('Horas:'))
-ano = 365
+anos = int(input('Anos:'))
+
+um_ano = 365
 ano_hora = 8766
 
-hora_ano = hora / ano_hora
+hora_p_ano = hora / ano_hora
+ano_p_hora = anos * ano_hora
 
-print(hora_ano)
+print(hora_p_ano)
+print(ano_p_hora)
+
