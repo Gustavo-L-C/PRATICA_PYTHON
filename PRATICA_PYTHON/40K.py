2fc7ec0ec06d382a44341dd83649363b933bfd5f
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2026-01-20 12:50:53 -0300

    Create 40K.py

---
+++ PRATICA_PYTHON/40K.py
@@ -0,0 +1,26 @@
+#WARHAMMER 40K CHAPTER SELECTOR RANDOM
+
+import random
+from chapters40k import lista_chapters_D
+from chapters40k import lista_chapters
+from bancodenomes import sim_sim
+#from bancodenomes import nao_nao
+
+print('Seletor de Capitulo aleatorio para Space Marines 2\n')
+r = input('Pronto?')
+print('')
+
+if r in sim_sim:
+    rc = random.choice(lista_chapters_D)
+    print(f'Capitulo {rc}')
+
+    if rc == 'DeathWatch':
+        print(rc)
+        dw = input('Agora na DeathWatch, pronto para saber seu capitulo?')
+        if dw in sim_sim:
+            dc = random.choice(lista_chapters)
+            print(f'Capitulo {dc}')
+        else:
+            print(f'Então agora voce e um Black Shield da {rc}.')
+else:
+    print('Ok')
