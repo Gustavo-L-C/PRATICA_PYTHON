260e27d064ea5fbac915f61288488ce86e023dec
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2026-05-08 12:51:37 -0300

    Update 40K.py

--- PRATICA_PYTHON/40K.py
+++ PRATICA_PYTHON/40K.py
@@ -6,6 +6,7 @@ from chapters40k import lista_chapters
 from bancodenomes import sim_sim
 #from bancodenomes import nao_nao
 
+classe = ['TATICO','ASSALTO','VANGUARDA','BALUARTE','SNIPER','PESADO','TECHMARINE']
 print('Seletor de Capitulo aleatorio para Space Marines 2\n')
 r = input('Pronto?')
 print('')
@@ -27,3 +28,5 @@ if r in sim_sim:
 
 else:
     print('Ok')
+
+input('')
