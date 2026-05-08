b6865d51062a0628f26eae24cc60815747bb910c
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2026-05-08 13:25:28 -0300

    Update 40K.py

--- PRATICA_PYTHON/40K.py
+++ PRATICA_PYTHON/40K.py
@@ -6,7 +6,7 @@ from chapters40k import lista_chapters
 from bancodenomes import sim_sim
 #from bancodenomes import nao_nao
 
-classe = ['Tatico🔫','Assalto🚀','Vanguarda🗡️','Baluarte🏳️','Sniper🎯','Pesado🛡️','TECHMARINE⚙️']
+classe = ['Tatico🔫','Assalto🚀','Vanguarda🗡️','Baluarte🏳️','Sniper🎯','Pesado🛡️','Techmarine⚙️']
 
 print(f'\n', '-'*80) #só enfeite
 
