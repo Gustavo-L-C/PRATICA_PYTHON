754d6ff6e786cfc8abc075a3d24c3dad64f70557
Gustavo-Lourenco <git.gustavolourenco@gmail.com>
2026-05-09 10:47:09 -0300

    Create pratica59.py

---
+++ PRATICA_PYTHON/pratica59.py
@@ -0,0 +1,15 @@
+#09.05.26
+# SLEEP
+
+import time
+
+def funcao_um():
+    print("Iniciando função um...")
+
+def funcao_dois():
+    print("Iniciando função dois...")
+
+# Chamada das funções com delay
+funcao_um()
+time.sleep(2)  # Pausa por 2 segundos
+funcao_dois()
